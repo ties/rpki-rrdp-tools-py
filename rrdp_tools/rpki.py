eed95e729857dea1241ea143c71cf8186ad00d2e
Ties de Kock <ties@tiesdekock.nl>
2023-12-18 23:14:27 +0100

    Add manifest parsing

--- rrdp_tools/rpki.py
+++ rrdp_tools/rpki.py
@@ -1,25 +1,33 @@
 import logging
 from dataclasses import dataclass
 from datetime import datetime
+from pathlib import Path
 from typing import List
 
 from asn1crypto import cms, crl, x509
 
+import asn1tools
+
 LOG = logging.getLogger(__name__)
+
+asn1_src = Path(__file__).parent / "rfc9286.asn"
+assert asn1_src.exists()
+RFC_9286_ASN1 = asn1tools.compile_files(str(asn1_src), cache_dir='asn1')
 
 
 @dataclass
-class ManifestEntry:
+class FileAndHash:
     file_name: str
     hash: bytes
 
 @dataclass
-class SignedObjectInfo:
+class ManifestInfo:
     manifest_number: int
     signing_time: datetime
     ee_certificate: x509.Certificate
+    file_list: List[FileAndHash]
 
-def parse_signed_object(content: bytes) -> SignedObjectInfo:
+def parse_manifest(content: bytes) -> ManifestInfo:
     info = cms.ContentInfo.load(content)
     assert info['content_type'].native == 'signed_data'
     signed_data = info["content"]
@@ -30,9 +38,21 @@ def parse_signed_object(content: bytes) -> SignedObjectInfo:
     for attr in signer["signed_attrs"]:
         if attr["type"].native == "signing_time":
             signing_time = attr["values"][0].native
-        
-    breakpoint()
 
+    mft = RFC_9286_ASN1.decode('Manifest', signed_data['encap_content_info']['content'].native)
+
+    return ManifestInfo(
+        manifest_number=mft['manifestNumber'],
+        signing_time=signing_time,
+        ee_certificate=signed_data['certificates'][0].chosen,
+        file_list=[
+            FileAndHash(
+                file_name=entry['file'],
+                hash=entry['hash'][0]
+            )
+            for entry in mft['fileList']
+        ]
+    )
 
 
 def parse_file_time(file_name: str, content: bytes) -> datetime:
