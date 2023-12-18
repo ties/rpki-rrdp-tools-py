085e3adc516829f920dc56f3c94316ca49807808
Ties de Kock <ties@tiesdekock.nl>
2023-12-18 22:50:31 +0100

    WIP

--- rrdp_tools/rpki.py
+++ rrdp_tools/rpki.py
@@ -1,9 +1,38 @@
 import logging
+from dataclasses import dataclass
 from datetime import datetime
+from typing import List
 
 from asn1crypto import cms, crl, x509
 
 LOG = logging.getLogger(__name__)
+
+
+@dataclass
+class ManifestEntry:
+    file_name: str
+    hash: bytes
+
+@dataclass
+class SignedObjectInfo:
+    manifest_number: int
+    signing_time: datetime
+    ee_certificate: x509.Certificate
+
+def parse_signed_object(content: bytes) -> SignedObjectInfo:
+    info = cms.ContentInfo.load(content)
+    assert info['content_type'].native == 'signed_data'
+    signed_data = info["content"]
+    signer = signed_data["signer_infos"][0]
+
+    signing_time = None
+
+    for attr in signer["signed_attrs"]:
+        if attr["type"].native == "signing_time":
+            signing_time = attr["values"][0].native
+        
+    breakpoint()
+
 
 
 def parse_file_time(file_name: str, content: bytes) -> datetime:
